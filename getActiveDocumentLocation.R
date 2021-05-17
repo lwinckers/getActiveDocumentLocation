f56b75b7f0f23d6a61569f27bbc0e91b99f7cc51
LaurentW <laurent_2207@live.nl>
2021-05-17 16:33:14 +0200

    Initial commit

---
+++ getActiveDocumentLocation.R
@@ -0,0 +1,24 @@
+###
+###
+###
+
+library(tidyverse)
+library(dplyr)
+
+
+getCurrentFileLocation <-  function()
+{
+  this_file <- commandArgs() %>% 
+    tibble::enframe(name = NULL) %>%
+    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
+    dplyr::filter(key == "--file") %>%
+    dplyr::pull(value)
+  if (length(this_file)==0)
+  {
+    this_file <- rstudioapi::getSourceEditorContext()$path
+  }
+  return(dirname(this_file))
+}
+
+getCurrentFileLocation()
+
